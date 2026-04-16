31ffae2d67085e9b5c5d31e858426fb2182dcdd0
Uoops <semiswiet@gmail.com>
2026-04-15 22:57:05 -0700

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,21 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.data = [-1] * 1000001
+
+    def put(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data[key]
+
+    def remove(self, key):
+        self.data[key] = -1
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
